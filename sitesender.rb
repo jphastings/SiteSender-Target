6cc6571b7dc6fca04b5993b76adea46ad1f029ac
JP Hastings-Spital <jphastings@gmail.com>
2009-12-04 17:06:35 +0000

    Minor improvements

--- sitesender.rb
+++ sitesender.rb
@@ -10,9 +10,9 @@ settings = YAML.load(open("settings.yaml"))
 
 if not settings['port'].nil?
   set :port, settings['port']
-else
-  # Will choose a random port here and try to punch a hole in the NAT using UPnP
 end
+
+# Will choose a random port here and try to punch a hole in the NAT using UPnP
 
 begin
   data = open("http://sitesender.heroku.com/api/login?email=#{settings['email']}&password=#{settings['password']}&port=#{Sinatra::Application.port}").read
@@ -21,13 +21,13 @@ rescue
 end
 
 get '/' do
-  
+  redirect 'http://sitesender.heroku.com',303
 end
 
 get '/open' do
   content_type :json
   begin
-    raise UnofficialOpenRequestError, "A third party tried to ask your computer to open a website. It was prevented." if env['REMOTE_ADDR'] != "75.101.145.87"
+    raise UnofficialOpenRequestError, "A third party (#{env['REMOTE_ADDR']}) tried to ask your computer to open a website. It was prevented." if env['REMOTE_ADDR'] != "174.129.63.16"
     Launchy.open(params[:url])
     {:success => true}
   rescue
